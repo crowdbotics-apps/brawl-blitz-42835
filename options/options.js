05de620b5db270b816f94890a514ff435d608bc4
Crowdbotics <team@crowdbotics.com>
2023-08-14 06:38:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "brawl-blitz-42835",
+  url: "https://brawl-blitz-42835.botics.co",
+  api: "https://brawl-blitz-42835.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
